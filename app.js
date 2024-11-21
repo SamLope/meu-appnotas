2cc0550a914c393d0d5b6504fb16df3625ce201b
Samlope <samir.lopes@hotmail.com>
2024-11-21 19:34:00 -0300

    Adicionando meu nome

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let notes = [];
 
 // Rota principal
 app.get('/', (req, res) => {
-  res.send('Bem-vindo à API de Notas!');
+  res.send('Bem-vindo à API de Notas Samir!');
 });
 
 // Criar uma nova nota
